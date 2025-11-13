7a63209392e9c6e7c89f15b9a6b19853c1498c86
D-108 <therealdensentrated@gmail.com>
2025-11-13 12:19:37 -0500

    clean: add register IDs to DE1 class

--- DE1.h
+++ DE1.h
@@ -5,6 +5,9 @@ class DE1 {
     private:
         char *pBase;
         int fd;
+    protected:
+        const unsigned int HEX3_HEX0_BASE = 0x00000020; // HEX Reg1 offset
+        const unsigned int HEX5_HEX4_BASE = 0x00000030; // HEX Reg2 offset
     public:
         DE1();
         ~DE1();
