9950941df7ec8a5524849190b53ff63e05287872
Nico Constanti <156711146+Komo-Blows@users.noreply.github.com>
2025-12-03 11:32:21 -0500

    Lab 10

---
+++ lab10/Spider/Main.cpp
@@ -0,0 +1,85 @@
+/**
+ * @file   Main.cpp
+ * @Author John Kimani (j.kimani@northeastern.edu)
+ * @date   November, 2025
+ * @brief  Tests the Spider robot rc servo movements
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+#include "MMap.h"
+#include "Spider.h"
+#include "LEDControl.h" // Use as needed
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "\nProgram Starting...!" << endl; 
+	int speed = 0;
+	MMap *m_map = new MMap();
+    Spider *spider = new Spider(m_map);
+
+	cout << "Spider Init" << endl;
+	spider->Init();
+	
+	cout << "Spider Standup" << endl;	
+	spider->Standup();
+
+	cout << "Waiting for Command..." << endl;
+	cout << "t (reset), p (speed), q (stop), w (foward), a (left), s (back), d (right):" << endl;
+	bool done = false;
+	while (!done)
+	{
+		char cmd_chr;
+		cout << "Enter Next Command: ";
+		cin >> cmd_chr;
+		
+		switch (cmd_chr)
+		{
+			case 't':
+				cout << "Reset..." << endl;
+				spider->Reset();
+				break;
+			case 'p':
+				cout << "Set speed..." << endl;
+				cout << "Enter new speed [0 - 100]: ";
+				cin >> speed;
+				spider->SetSpeed(speed);
+				break;
+			case 'w':
+				cout << "Move Forward..." << endl;
+				spider->MoveForward(1);
+				break;
+			case 'a':
+				cout << "Rotate Left..." << endl;
+				spider->RotateLeft(1);
+				break;
+			case 's':
+				cout << "Move Backward..." << endl;
+				spider->MoveBackward(1);
+				break;
+			case 'd':
+				cout << "Rotate Right..." << endl;
+				spider->RotateRight(1);
+				break;
+			case 'q':
+				cout << "STOP Spider" << endl;
+				done = true;
+				break;			
+			default:
+				cout << "IDLE or UNKNOWN COMMAND" << endl;
+				break;
+		}
+	}
+	cout << "Spider Reset" << endl;	
+	spider->Reset();
+	delete spider;
+	delete m_map;
+	return 0;
+}
+
---
+++ lab10/Spider/Spider.h
@@ -0,0 +1,67 @@
+ /**
+ * @file   Spider.h
+ * @Author Richard, Modified by: John Kimani (j.kimani@northeastern.edu)
+ * @date   Modified on: 11/01/2025
+ * @brief  Process Spider robot with coordinated leg movements
+ *
+ * Contains a Spider class that provides functions to operate
+ * on the six spider legs on the Terasic Spider robot
+ */
+
+#ifndef SPIDER_H_
+#define SPIDER_H_
+
+#include "MMap.h"
+#include "SpiderLeg.h"
+
+/*
+*	Class Spider
+*/
+class Spider{
+private:
+	// Define an enumertion for the 6 legs
+	typedef enum{
+		LEG_RF,
+		LEG_RM,
+		LEG_RB,
+		LEG_LF,
+		LEG_LM,
+		LEG_LB,
+		LEG_NUM
+	}LEG_ID;
+
+	// Define the two tripods
+	typedef enum{
+		TRIPOD1,    //RF LM RB
+		TRIPOD2,	//LF RM LB
+		TRIPOD_NUM
+	}TRIPOD_ID;
+	
+	typedef enum{
+		FWD,
+		BACK
+	} DIR;
+
+	// A SpiderLeg array of the six legs
+	SpiderLeg *m_szLeg[LEG_NUM];
+	MMap* m_map;		// Memory map object
+	TRIPOD_ID lastStep;
+	DIR lastDir;
+
+public:
+	Spider(MMap* mmio);		// Constructor
+	~Spider();				// Destructor
+
+	bool IsReady(void);
+	bool WaitReady(void);
+	void MoveTripod(TRIPOD_ID Tripod,SpiderLeg::JOINT_ID Joint,float AngleF,float AngleM,float AngleB);
+	//Action
+	bool Init(void);
+	void Reset(void);
+	bool Standup(void);
+	void SetSpeed(int Speed);
+	void MoveForward(uint8_t Repeat_Num);
+	void MoveBackward(uint8_t Repeat_Num);
+};
+
+#endif /* CSPIDER_H_ */
