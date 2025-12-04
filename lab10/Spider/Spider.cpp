e6dc59671faa856f539603062cf409b9b835870d
Nico Constanti <156711146+Komo-Blows@users.noreply.github.com>
2025-12-04 11:09:20 -0500

    Update Spider.cpp

--- lab10/Spider/Spider.cpp
+++ lab10/Spider/Spider.cpp
@@ -220,14 +220,14 @@ void Spider::RotateLeft()
 	MoveTripod(TRIPOD1, SpiderLeg::Knee, Knee_Up_Base, Knee_Up_Base, Knee_Up_Base); // lift tripod 1
 	WaitReady();
 	// move tripod 1 to leftmost position
-	MoveTripod(TRIPOD1, SpiderLeg::Hip, HipF_Base + 20, HipM_Base - 20, HipB_Base - 20); // tripod 2 rotate right
+	MoveTripod(TRIPOD1, SpiderLeg::Hip, HipF_Base + 20, HipM_Base - 20, HipB_Base + 20); // tripod 2 rotate right
 	WaitReady();
 	// drop tripod 1, lift tripod 2
 	MoveTripod(TRIPOD1, SpiderLeg::Knee, Knee_Down_Base, Knee_Down_Base, Knee_Down_Base); // drop tripod 2
 	MoveTripod(TRIPOD2, SpiderLeg::Knee, Knee_Up_Base, Knee_Up_Base, Knee_Up_Base); // lift tripod 1
 	WaitReady();
 	// rotate tripod 1 to the right (rotates spider left)
-	MoveTripod(TRIPOD1, SpiderLeg::Hip, HipF_Base - 20, HipM_Base + 20, HipB_Base + 20); // tripod 2 rotate right
+	MoveTripod(TRIPOD1, SpiderLeg::Hip, HipF_Base - 20, HipM_Base + 20, HipB_Base - 20); // tripod 2 rotate right
 	WaitReady();
 	MoveTripod(TRIPOD2, SpiderLeg::Knee, Knee_Down_Base, Knee_Down_Base, Knee_Down_Base); // drop tripod 2
 	MoveTripod(TRIPOD1, SpiderLeg::Knee, Knee_Down_Base, Knee_Down_Base, Knee_Down_Base); // drop tripod 1
